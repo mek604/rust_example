f06039d9423946be62f90055069aabc5cdf41ae0
Mek O <mek.arnuparp@gmail.com>
2022-01-15 13:49:37 -0800

    added for_each example

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,13 @@ use std::str::FromStr;
 use lib::*;
 
 // src/main.rs root of binary crate
+
 fn main() {
 
     let numbers = read_u64();
     let d = math::gcd(&numbers);
 
     println!("The gcd of {:?} is {}", numbers, d);
-
 }
 
 
@@ -19,7 +19,6 @@ fn main() {
 #[test]
 fn test_gcd() {
     assert_eq!(gcd(14, 15), 1);
-
     assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19, 3 * 11));
 }
 
@@ -28,8 +27,14 @@ fn read_u64() -> Vec<u64> {
 
     for arg in std::env::args().skip(1) {
         numbers.push(u64::from_str(&arg)
-            .expect("error parsing argument"));
+            .expect("error parsing argument")); 
+        // if panics, error msg begins with "error parsing argument"
+        // if not, return contained value
     }
+
+    // one-liner
+    // std::env::args().skip(1).for_each(|n| numbers.push(u64::from_str(&n)
+    //     .expect("error parsing argument")));
 
     if numbers.len() == 0 {
         writeln!(std::io::stderr(), "Usage: gcd NUMBER ...").unwrap();
