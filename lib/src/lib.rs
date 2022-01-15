56272f0c031781ceead1db40e69f0f0d094178cc
Mek O <mek.arnuparp@gmail.com>
2022-01-15 13:25:53 -0800

    added an example of calculating the greatest common divisor
    ex.
    $cargo run 5 10 15 20
    > the gcd of [5, 10, 15, 20] is 5

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,5 +1,27 @@
+
 pub mod math {
-    pub fn print() {
-        println!("Hello, world!");
+
+    fn xy_gcd(mut n: u64, mut m: u64) -> u64 {
+         // Rust always checks assertion!
+         // debug_assert! is skipped when compiled
+        assert!(n != 0 && m != 0);
+        while m != 0 {
+            if m < n {
+                let t = m; // type inference, can only happen within function body
+                m = n;
+                n = t;
+            }
+            m = m % n;
+        }
+        n 
     }
+
+    pub fn gcd(numbers: &Vec<u64>) -> u64 {
+        let mut d = numbers[0];
+        for m in &numbers[1..] {
+            d = xy_gcd(d, *m);
+        }
+        d
+    }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,40 @@
-use lib::math;
+use std::io::Write;
+use std::str::FromStr;
 
+use lib::*;
+
+// src/main.rs root of binary crate
 fn main() {
-    math::print();
+
+    let numbers = read_u64();
+    let d = math::gcd(&numbers);
+
+    println!("The gcd of {:?} is {}", numbers, d);
+
+}
+
+
+// #[test] attribute, marks fn as a test fn - skipped in normal compilations
+// include only when run cargo test
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(14, 15), 1);
+
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19, 3 * 11));
+}
+
+fn read_u64() -> Vec<u64> {
+    let mut numbers = Vec::new();
+
+    for arg in std::env::args().skip(1) {
+        numbers.push(u64::from_str(&arg)
+            .expect("error parsing argument"));
+    }
+
+    if numbers.len() == 0 {
+        writeln!(std::io::stderr(), "Usage: gcd NUMBER ...").unwrap();
+        std::process::exit(1);
+    }
+
+    numbers    
 }
